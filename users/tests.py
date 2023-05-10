898373d1fd243e6ac850378d628ecb0d6ff665ca
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-10 14:53:26 +0900

    add signup test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class UserRegistrationAPIViewTestCase(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "email":"one@gmail.com",
+            "password":"1",
+            "username":"one",
+            "gender":"F",
+            "date_of_birth":"2001-01-01",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.status_code, 201)
