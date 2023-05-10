aefbae2eef78ba427eb5c08284d1a89c3283f7cb
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-10 15:32:34 +0900

    add login test

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,10 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from users.models import User
 
 
-class UserRegistrationAPIViewTestCase(APITestCase):
+class UserRegistrationTest(APITestCase):
     def test_registration(self):
         url = reverse("user_view")
         user_data = {
@@ -15,3 +16,19 @@ class UserRegistrationAPIViewTestCase(APITestCase):
         }
         response = self.client.post(url, user_data)
         self.assertEqual(response.status_code, 201)
+
+
+class UserLoginTest(APITestCase):
+    def setUp(self):
+
+        self.data = {
+            "email":"one@gmail.com",
+            "password":"1",
+        }
+        self.user = User.objects.create_user("one@gmail.com", "one", "1")
+        return super().setUp()
+
+    def test_login(self):
+        url = reverse("token_obtain_pair")
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, 200)
